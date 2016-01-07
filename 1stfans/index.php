bd486311c9a354071d5d99326db07e94ca4dcdfe
Lauren McCarthy <laurmccarthy@gmail.com>
2016-01-06 18:43:36 -0800

    s

---
+++ 1stfans/index.php
@@ -0,0 +1,36 @@
+<?php include "../header.php"; ?>
+
+<div id="content">
+  <p class="summary"><span class="red">Showertweet.</span> In August, I was the Twitter Artist in Residence for the Brooklyn Museum of Art. I waterproofed my phone and tweeted everyday from my shower.</p>
+  <div id="description">
+  <p class="description">For me, the shower is a place for private, intimate reflection and ritual - a time all for myself to focus on my body and my thoughts without interruption. It is a preparation for the day, physically and psychologically. It is a set of routine motions that I can do with little attention to them, leaving my mind free to wander.<br/><br/>
+    
+    I will create a waterproof casing for my phone [more photos of this process may be found in the 1stfans flickr group], and send one tweet via text message per day from inside the shower. The subject of the tweets will be whatever thoughts come to mind as I wash myself. They will mix observations of the physical sensations of cleaning my body with thoughts that arise through the process.<br/><br/>
+    
+    The practice will explore the way twitter/technology is incorporated into daily ritual, and provide a moment for me to focus on the significance and experience of cleaning myself, while engaging simultaneously with my physical body and my mental processes. Others will be able to share in this, as my normally private shower reflection is made public through the twitter broadcast.<br/><br/>
+    
+    Each person has their own personal shower ritual and it is rarely given much thought or discussed. Through this project I would hope to inspire others to take a minute while showering to consciously consider their routines, their bodies, and their ideas.<br/><br/>
+    
+    I also want to explore the effect technology has on the boundary between private and public life. What happens if the most intimate experiences are shared online? How is this sharing different than if it were to occur in physical space? What place do our physical bodies have in our increasingly technologically mediated world?</p>
+    <div id="backLink"><a class="back" href="../projects.html">back to projects</a></div>
+  </div>
+  
+<div id="tallImage">
+  <img src='./1stfans2.jpg'/>
+  <img src='./1stfans0.jpg'/>
+  <img src='./tweets.jpg' />  </div>
+    </div>
+</div>
+
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6271620-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
---
+++ artiststudio/index.php
@@ -0,0 +1,43 @@
+<?php include "../header.php"; ?>
+
+<div id="content">
+<p class="title">ARTIST STUDIO <span class="date">2007</span></p>
+<p class="summary">The overlapping, reconfigurable glass walls responded to the site, while allowing the artist to determine the boundaries between public and private space.</p>
+<div id="description">
+<p class="description">Creating a place for an artist to live, work, and interact with the public as a part of the redevelopment of Boston's Piers was an exciting project. Drawing inspiration from the reflective waves of the surrounding water, I made all the walls glass. An important consideration for the design was the distinction between private and public space. Parts of the walls were translucent and other parts were transparent, and the interior walls slid on tracks. This reconfigurable quality allowed each individual artist to choose what was visible to the public and what was private.</p>
+
+
+
+
+<div id="backLink"><a class="back" href="../projects.html">back to projects</a></div>
+
+</div>
+
+<div id="tallImage">
+<img src='./artiststudio0.jpg'/>
+<img src='./artiststudio1.jpg'/>
+<img src='./artiststudio2.jpg'/>
+<img src='./artiststudio3.jpg'/>
+<img src='./artiststudio4.jpg'/>
+<img src='./artiststudio5.jpg'/>
+<img src='./artiststudio6.jpg'/>
+</div>
+
+</div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6271620-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
+
+
+
+
+
